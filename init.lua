150897ba1bbd81bc8c924cd8aadb7f90a8966d65
Nico Bugash <vnonicob@audible.com>
2025-02-20 14:29:40 -0500

    Added hard mode

--- init.lua
+++ init.lua
@@ -1022,3 +1022,25 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+--
+-- Enable Hard Mode
+local hardmode = true
+if hardmode then
+  -- Show an error message if a disabled key is pressed
+  local msg = [[<cmd>echohl Error | echo "KEY DISABLED" | echohl None<CR>]]
+
+  -- Disable arrow keys in insert mode with a styled message
+  vim.api.nvim_set_keymap('i', '<Up>', '<C-o>' .. msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('i', '<Down>', '<C-o>' .. msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('i', '<Left>', '<C-o>' .. msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('i', '<Right>', '<C-o>' .. msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('i', '<Del>', '<C-o>' .. msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('i', '<BS>', '<C-o>' .. msg, { noremap = true, silent = false })
+
+  -- Disable arrow keys in normal mode with a styled message
+  vim.api.nvim_set_keymap('n', '<Up>', msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('n', '<Down>', msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('n', '<Left>', msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('n', '<Right>', msg, { noremap = true, silent = false })
+  vim.api.nvim_set_keymap('n', '<BS>', msg, { noremap = true, silent = false })
+end
